7c9c7656c85ddb6c9f166ad8ac7eb582ae0ace72
rique00779 santos <contatoarthurhenrique050@gmail.com>
2024-06-11 15:11:44 +0000

    java script do index

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-let botoes = document.querySelectorAll(".botoes buttom")
+let botoes = document.querySelectorAll(".botoes button")
 botoes.forEach(botao => {
-botao.addEventListener("click", guardarNota)
-
+    botao.addEventListener("click", guardarNota)
 })
-    
+
 function guardarNota(evento) {
- let nota = evento.target.innertext
+   let nota = evento.target.innerText
 
-localStorage.setItem("nota", nota)
-
+   localStorage.setItem("nota", nota)
 }
-
-
-
